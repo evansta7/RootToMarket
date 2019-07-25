976d419d0fa7388f745394accefb664e9477a3e5
evansta7 <lizy.vs@gmail.com>
2019-07-25 17:00:01 +0200

    initial commit

---
+++ SharedContent.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Root To Market</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> 
+
+  </head>
+<body>
+    <div class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+               <h3><a href="/RootToMarket/index.html">Root To Market</a></h3>
+            </div>
+                <ul class="nav navbar-nav">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>
+                        <a href="#">Products<b></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Arctic Store</a></li>
+                            <li><a href="#">Fats and Oils</a></li>
+                            <li><a href="#">Batteries</a></li>
+                            <li><a href="#">Bakery</a></li>
+                        </ul>
+                    </li>
+                    <li>Contact</li>
+
+                </ul>
+        </div>
+    </div>
+    <div class="container body-content">
+       
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Root To Market</title>
+  <meta charset="utf-8">    
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php include 'sharedContent.php'?>
+  </head>
+</html>
+<body>
+<fieldset id="slideshow">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div id="imageCarousel" class="carousel slide" data-interval="5000"
+                     data-ride="carousel" data-pause="hover" data-wrap="true">
+
+                    <ol class="carousel-indicators">
+                        <li data-target="#imageCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#imageCarousel" data-slide-to="1"></li>
+                        <li data-target="#imageCarousel" data-slide-to="2"></li>
+                    </ol>
+
+                    <div class="carousel-inner">
+                        <div class="item active">
+                            <img src="Images/1.png" class="img-responsive" id="img1">
+                            <div class="carousel-caption">
+                                <h3></h3>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="Images/2.png" class="img-responsive" id="img2">
+                            <div class="carousel-caption">
+                                <h3></h3>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="Images/3.png" class="img-responsive" id="img3">
+                            <div class="carousel-caption">
+                                <h3></h3>
+                            </div>
+                        </div>
+                    </div>
+
+                    <a href="#imageCarousel" class="carousel-control left" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left"></span>
+                    </a>
+                    <a href="#imageCarousel" class="carousel-control right" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right"></span>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</fieldset>
+<div class="row">
+    <div class="col-md-4">
+        <h2>Getting started</h2>
+        <p>
+            ASP.NET MVC gives you a powerful, patterns-based way to build dynamic websites that
+            enables a clean separation of concerns and gives you full control over markup
+            for enjoyable, agile development.
+        </p>
+        <p><a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301865">Learn more &raquo;</a></p>
+    </div>
+    <div class="col-md-4">
+        <h2>Get more libraries</h2>
+        <p>NuGet is a free Visual Studio extension that makes it easy to add, remove, and update libraries and tools in Visual Studio projects.</p>
+        <p><a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301866">Learn more &raquo;</a></p>
+    </div>
+    <div class="col-md-4">
+        <h2>Web Hosting</h2>
+        <p>You can easily find a web hosting company that offers the right mix of features and price for your applications.</p>
+        <p><a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301867">Learn more &raquo;</a></p>
+    </div>
+</div>
+
+</body>
+

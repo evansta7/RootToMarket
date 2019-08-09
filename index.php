6363ddd7221d5bf1703fed2e873c669946aa2f3b
evansta7 <lizy.vs@gmail.com>
2019-08-09 16:15:29 +0200

    Commit

--- index.php
+++ index.php
@@ -11,7 +11,6 @@
   <?php include 'sharedContent.php'?>
   </head>
 </html>
-<h1>Hello</h1>
 <body>
      <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">Root To Market</a>

0e3e8219b9845c1548092141e3c4f7a5883fb626
evansta7 <lizy.vs@gmail.com>
2019-08-09 18:14:20 +0200

    SLideshow

--- index.php
+++ index.php
@@ -4,56 +4,53 @@
     <title>Root To Market</title>
   <meta charset="utf-8">    
   <meta name="viewport" content="width=device-width, initial-scale=1">
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="Style.css" />
+
   <?php include 'SharedContent.php'?>
   </head>
 </html>
 <body>
-<fieldset id="slideshow">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div id="imageCarousel" class="carousel slide" data-interval="5000"
-                     data-ride="carousel" data-pause="hover" data-wrap="true">
-
-                    <ol class="carousel-indicators">
-                        <li data-target="#imageCarousel" data-slide-to="0" class="active"></li>
-                        <li data-target="#imageCarousel" data-slide-to="1"></li>
-                        <li data-target="#imageCarousel" data-slide-to="2"></li>
-                    </ol>
-
-                    <div class="carousel-inner">
-                        <div class="item active">
-                            <img src="Images/1.png" class="img-responsive" id="img1">
-                            <div class="carousel-caption">
-                                <h3></h3>
-                            </div>
-                        </div>
-                        <div class="item">
-                            <img src="Images/2.png" class="img-responsive" id="img2">
-                            <div class="carousel-caption">
-                                <h3></h3>
-                            </div>
-                        </div>
-                        <div class="item">
-                            <img src="Images/3.png" class="img-responsive" id="img3">
-                            <div class="carousel-caption">
-                                <h3></h3>
-                            </div>
-                        </div>
-                    </div>
-
-                    <a href="#imageCarousel" class="carousel-control left" data-slide="prev">
-                        <span class="glyphicon glyphicon-chevron-left"></span>
-                    </a>
-                    <a href="#imageCarousel" class="carousel-control right" data-slide="next">
-                        <span class="glyphicon glyphicon-chevron-right"></span>
-                    </a>
-                </div>
-
-            </div>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel"  data-interval="4000">
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="item bg bg3 active">
+        <div class="container">
+          <div class="carousel-caption">
+            <h1>Example headline.</h1>
+            <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
+            <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+          </div>
         </div>
+      </div>
+      <div class="item bg bg2">
+        <div class="container">
+          <div class="carousel-caption">
+            <h1>Another example headline.</h1>
+            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="item bg bg1">
+        <div class="container">
+          <div class="carousel-caption">
+            <h1>One more for good measure.</h1>
+            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+          </div>
+        </div>
+      </div>
     </div>
-</fieldset>
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+  </div>
 <div class="row">
     <div class="col-md-4">
         <h2>Getting started</h2>
